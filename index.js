970c723c6d59c818ea107daafb1e296c7ef47719
TIMUR <otto@MacBook-Air-Timur.local>
2022-08-18 17:22:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-export const getRandomNumberInRange = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
+export const greeting = () => console.log('Welcome to the Brain Games!');
 
-export const getPrimeNumber = (number) => {
-    let result = 0;
-  
-    for (let i = number; i > 0; i -= 1) {
-      if (number % i === 0) {
-        result = result + 1;
-      } else {
-        continue;
-      }
-    };
-  
-    if (result === 2) {
-      return true;
-    } else {
-      return false;
-    }
-  };
+export const randomNumberInRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
