c9bc032bfa69eeac159ca3ce23f253fb730e0413
TIMUR <otto@MacBook-Air-Timur.local>
2022-08-20 21:02:30 +0300

    Update bin/brain-progression.js

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import runBrainProgression from '../games/game-progression.js';
+import runBrainProgression from '../src/games/game-progression.js';
 
 console.log(runBrainProgression());
