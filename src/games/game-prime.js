68e4896b8412b9307bbde8e52b5129ffc7bc4f42
TIMUR <otto@MacBook-Air-Timur.local>
2022-08-22 22:14:15 +0300

    Fix src/games/game-prime.js

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -1,68 +1,49 @@
-import readlineSync from 'readline-sync';
-import { greeting, randomNumberInRange } from '../../index.js';
+import { randomNumberInRange } from '../../index.js';
 
-const runBrainPrime = () => {
-  greeting();
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+export const rulesOfGame = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-  const getPrimeNumber = (number) => {
-    let result = 0;
+const getPrimeNumber = (number) => {
+  let result = 0;
 
-    for (let i = number; i > 0; i -= 1) {
-      if (number % i === 0) {
-        result += 1;
-      }
+  for (let i = number; i > 0; i -= 1) {
+    if (number % i === 0) {
+      result += 1;
     }
-
-    if (result === 2) {
-      return true;
-    }
-    return false;
-  };
-
-  const firstNumberRange = 1;
-  const lastNumberRange = 25;
-  let randomNumber = randomNumberInRange(firstNumberRange, lastNumberRange);
-  let correctAnswer;
-
-  console.log(`Question: ${randomNumber}`);
-  let userAnswer = readlineSync.question('Your answer: ');
-  if (getPrimeNumber(randomNumber) === true) {
-    correctAnswer = 'yes';
-  } else if (getPrimeNumber(randomNumber) === false) {
-    correctAnswer = 'no';
   }
-  if (userAnswer === correctAnswer) {
-    console.log('Correct!');
 
-    randomNumber = randomNumberInRange(firstNumberRange, lastNumberRange);
-    console.log(`Question: ${randomNumber}`);
-    userAnswer = readlineSync.question('Your answer: ');
-    if (getPrimeNumber(randomNumber) === true) {
-      correctAnswer = 'yes';
-    } else if (getPrimeNumber(randomNumber) === false) {
-      correctAnswer = 'no';
-    }
-    if (userAnswer === correctAnswer) {
-      console.log('Correct!');
-
-      randomNumber = randomNumberInRange(firstNumberRange, lastNumberRange);
-      console.log(`Question: ${randomNumber}`);
-      userAnswer = readlineSync.question('Your answer: ');
-      if (getPrimeNumber(randomNumber) === true) {
-        correctAnswer = 'yes';
-      } else if (getPrimeNumber(randomNumber) === false) {
-        correctAnswer = 'no';
-      }
-      if (userAnswer === correctAnswer) {
-        return `Correct!\nCongratulations, ${userName}!`;
-      }
-      return `'${userAnswer.toString()}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`;
-    }
-    return `'${userAnswer.toString()}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`;
+  if (result === 2) {
+    return true;
   }
-  return `'${userAnswer.toString()}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`;
+  return false;
 };
-export default runBrainPrime;
+
+const firstNumberRange = 1;
+const lastNumberRange = 25;
+const randomNumber = randomNumberInRange(firstNumberRange, lastNumberRange);
+const secondRandomNumber = randomNumberInRange(firstNumberRange, lastNumberRange);
+const thirdRandomNumber = randomNumberInRange(firstNumberRange, lastNumberRange);
+
+let correctAnswer;
+if (getPrimeNumber(randomNumber) === true) {
+  correctAnswer = 'yes';
+} else if (getPrimeNumber(randomNumber) === false) {
+  correctAnswer = 'no';
+}
+const answerOne = correctAnswer;
+
+if (getPrimeNumber(secondRandomNumber) === true) {
+  correctAnswer = 'yes';
+} else if (getPrimeNumber(secondRandomNumber) === false) {
+  correctAnswer = 'no';
+}
+const answerTwo = correctAnswer;
+
+if (getPrimeNumber(thirdRandomNumber) === true) {
+  correctAnswer = 'yes';
+} else if (getPrimeNumber(thirdRandomNumber) === false) {
+  correctAnswer = 'no';
+}
+const answerThree = correctAnswer;
+
+export const arrayQuestions = [randomNumber, secondRandomNumber, thirdRandomNumber];
+export const arrayAnswers = [answerOne, answerTwo, answerThree];
