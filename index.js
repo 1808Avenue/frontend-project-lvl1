fccb9a144a4c57e0178a241ec03ac70b056254c0
TIMUR <otto@MacBook-Air-Timur.local>
2022-08-22 22:05:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
 import readlineSync from 'readline-sync';
 
-export const greeting = () => console.log('Welcome to the Brain Games!');
-export const randomNumberInRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-
 export const brainGamesCore = (rules, question, correctAnswer) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(rules);
-
-  console.log(`Question: ${question}`);
-  let userAnswer = readlineSync.question('Your answer: ');
-  if (userAnswer === correctAnswer) {
-    console.log('Correct!');
-    console.log(`Question: ${question}`);
+  let userAnswer;
+  for (let i = 0; i < question.length; i += 1) {
+    console.log(`Question: ${question[i]}`);
     userAnswer = readlineSync.question('Your answer: ');
-
-    if (userAnswer === correctAnswer) {
+    if (userAnswer === correctAnswer[i]) {
       console.log('Correct!');
-      console.log(`Question: ${question}`);
-      userAnswer = readlineSync.question('Your answer: ');
-
-      if (userAnswer === correctAnswer) {
-        return console.log(`Correct!\nCongratulations, ${userName}!`);
-      } return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
-    } return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
-  } return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
+    } else {
+      return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer[i]}'.\nLet's try again, ${userName}!`);
+    }
+  }
+  return console.log(`Congratulations, ${userName}!`);
 };
+
+export const randomNumberInRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
