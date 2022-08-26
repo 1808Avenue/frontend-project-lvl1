c94fed9a400f7e50a10caea690e2b2ec71754f7e
TIMUR <otto@MacBook-Air-Timur.local>
2022-08-26 17:34:40 +0300

    Update src/games/game-calc.js

--- src/games/game-calc.js
+++ src/games/game-calc.js
@@ -1,71 +1,37 @@
-import { randomNumberInRange } from '../../index.js';
+import { brainGamesCore, randomNumberInRange } from '../index.js';
 
-export const rulesOfGame = 'What is the result of the expression?';
+const rulesOfGame = 'What is the result of the expression?';
 
-const firstNumberRange = 1;
-const lastNumberRange = 25;
-let firstRandomNumber = randomNumberInRange(firstNumberRange, lastNumberRange);
-let secondRandomNumber = randomNumberInRange(firstNumberRange, lastNumberRange);
+const getArrayQuestionAnswer = () => {
+  const firstNumberRange = 1;
+  const lastNumberRange = 25;
+  const firstRandomNumber = randomNumberInRange(firstNumberRange, lastNumberRange);
+  const secondRandomNumber = randomNumberInRange(firstNumberRange, lastNumberRange);
 
-const operator = ['+', '-', '*'];
-const firstOperatorIndex = 0;
-const lastOperatorIndex = operator.length - 1;
-let randomOperator = operator[randomNumberInRange(firstOperatorIndex, lastOperatorIndex)];
+  const operator = ['+', '-', '*'];
+  const firstOperatorIndex = 0;
+  const lastOperatorIndex = operator.length - 1;
+  const randomOperator = operator[randomNumberInRange(firstOperatorIndex, lastOperatorIndex)];
 
-const questionOne = `${firstRandomNumber} ${randomOperator} ${secondRandomNumber}`;
-let result;
-switch (randomOperator) {
-  case '+':
-    result = firstRandomNumber + secondRandomNumber;
-    break;
-  case '-':
-    result = firstRandomNumber - secondRandomNumber;
-    break;
-  case '*':
-    result = firstRandomNumber * secondRandomNumber;
-    break;
-  default:
-    throw new Error(`Unknown state: '${randomOperator}'!`);
-}
-const answerOne = result.toString();
+  const question = `${firstRandomNumber} ${randomOperator} ${secondRandomNumber}`;
+  let result;
+  switch (randomOperator) {
+    case '+':
+      result = firstRandomNumber + secondRandomNumber;
+      break;
+    case '-':
+      result = firstRandomNumber - secondRandomNumber;
+      break;
+    case '*':
+      result = firstRandomNumber * secondRandomNumber;
+      break;
+    default:
+      throw new Error(`Unknown state: '${randomOperator}'!`);
+  }
+  const answer = result.toString();
 
-firstRandomNumber = randomNumberInRange(firstNumberRange, lastNumberRange);
-secondRandomNumber = randomNumberInRange(firstNumberRange, lastNumberRange);
-randomOperator = operator[randomNumberInRange(firstOperatorIndex, lastOperatorIndex)];
-const questionTwo = `${firstRandomNumber} ${randomOperator} ${secondRandomNumber}`;
-switch (randomOperator) {
-  case '+':
-    result = firstRandomNumber + secondRandomNumber;
-    break;
-  case '-':
-    result = firstRandomNumber - secondRandomNumber;
-    break;
-  case '*':
-    result = firstRandomNumber * secondRandomNumber;
-    break;
-  default:
-    throw new Error(`Unknown state: '${randomOperator}'!`);
-}
-const answerTwo = result.toString();
+  const array = [question, answer];
+  return array;
+};
 
-firstRandomNumber = randomNumberInRange(firstNumberRange, lastNumberRange);
-secondRandomNumber = randomNumberInRange(firstNumberRange, lastNumberRange);
-randomOperator = operator[randomNumberInRange(firstOperatorIndex, lastOperatorIndex)];
-const questionThree = `${firstRandomNumber} ${randomOperator} ${secondRandomNumber}`;
-switch (randomOperator) {
-  case '+':
-    result = firstRandomNumber + secondRandomNumber;
-    break;
-  case '-':
-    result = firstRandomNumber - secondRandomNumber;
-    break;
-  case '*':
-    result = firstRandomNumber * secondRandomNumber;
-    break;
-  default:
-    throw new Error(`Unknown state: '${randomOperator}'!`);
-}
-const answerThree = result.toString();
-
-export const arrayQuestions = [questionOne, questionTwo, questionThree];
-export const arrayAnswers = [answerOne, answerTwo, answerThree];
+export const runBrainCalc = () => brainGamesCore(rulesOfGame, getArrayQuestionAnswer());
