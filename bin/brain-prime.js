a909e0205b1015f62faaf9f3ed9fb551de46600e
TIMUR <otto@MacBook-Air-Timur.local>
2022-08-28 18:02:38 +0300

    Update bin/brain-prime.js

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-import { brainGamesCore } from '../index.js';
-import { rulesOfGame, arrayQuestions, arrayAnswers } from '../src/games/game-prime.js';
+import runGamePrime from '../src/games/game-prime.js';
 
-brainGamesCore(rulesOfGame, arrayQuestions, arrayAnswers);
+runGamePrime();
