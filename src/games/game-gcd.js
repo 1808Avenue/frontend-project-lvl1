25d422bb5e49e1aa53c2a4ca2ee4303649d54f15
TIMUR <otto@MacBook-Air-Timur.local>
2022-08-28 17:56:44 +0300

    Fix src/games/game-gcd.js

--- src/games/game-gcd.js
+++ src/games/game-gcd.js
@@ -1,41 +1,38 @@
-import { randomNumberInRange } from '../../index.js';
+import { brainGamesCore, randomNumberInRange } from '../index.js';
 
-export const rulesOfGame = 'Find the greatest common divisor of given numbers.';
+const rulesOfGame = 'Find the greatest common divisor of given numbers.';
 
-const getGcd = (a, b) => {
-  let firstElement = a;
-  let secondElement = b;
+const getArrayQuestionAnswer = () => {
+  const getGcd = (a, b) => {
+    let firstElement = a;
+    let secondElement = b;
 
-  while (firstElement !== secondElement) {
-    if (a === 0) {
-      return b;
-    } if (b === 0) {
-      return a;
-    } if (firstElement > secondElement) {
-      firstElement -= secondElement;
-    } else {
-      secondElement -= firstElement;
+    while (firstElement !== secondElement) {
+      if (a === 0) {
+        return b;
+      } if (b === 0) {
+        return a;
+      } if (firstElement > secondElement) {
+        firstElement -= secondElement;
+      } else {
+        secondElement -= firstElement;
+      }
     }
-  }
-  return firstElement;
+    return firstElement;
+  };
+
+  const firstNumberRange = 1;
+  const lastNumberRange = 25;
+  const firstRandomNumber = randomNumberInRange(firstNumberRange, lastNumberRange);
+  const secondRandomNumber = randomNumberInRange(firstNumberRange, lastNumberRange);
+
+  const question = `${firstRandomNumber} ${secondRandomNumber}`;
+  const answer = getGcd(firstRandomNumber, secondRandomNumber).toString();
+
+  const arrayQuestionAnswer = [question, answer];
+  return arrayQuestionAnswer;
 };
 
-const firstNumberRange = 1;
-const lastNumberRange = 25;
-let firstRandomNumber = randomNumberInRange(firstNumberRange, lastNumberRange);
-let secondRandomNumber = randomNumberInRange(firstNumberRange, lastNumberRange);
-const questionOne = `${firstRandomNumber} ${secondRandomNumber}`;
-const answerOne = getGcd(firstRandomNumber, secondRandomNumber).toString();
+const runGameGcd = () => brainGamesCore(rulesOfGame, getArrayQuestionAnswer);
 
-firstRandomNumber = randomNumberInRange(firstNumberRange, lastNumberRange);
-secondRandomNumber = randomNumberInRange(firstNumberRange, lastNumberRange);
-const questionTwo = `${firstRandomNumber} ${secondRandomNumber}`;
-const answerTwo = getGcd(firstRandomNumber, secondRandomNumber).toString();
-
-firstRandomNumber = randomNumberInRange(firstNumberRange, lastNumberRange);
-secondRandomNumber = randomNumberInRange(firstNumberRange, lastNumberRange);
-const questionThree = `${firstRandomNumber} ${secondRandomNumber}`;
-const answerThree = getGcd(firstRandomNumber, secondRandomNumber).toString();
-
-export const arrayQuestions = [questionOne, questionTwo, questionThree];
-export const arrayAnswers = [answerOne, answerTwo, answerThree];
+export default runGameGcd;
