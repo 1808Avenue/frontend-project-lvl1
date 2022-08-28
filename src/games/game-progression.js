77623f57130829e7232134c70f46e592cedc6714
TIMUR <otto@MacBook-Air-Timur.local>
2022-08-28 17:59:23 +0300

    Fix src/games/game-progression.js

--- src/games/game-progression.js
+++ src/games/game-progression.js
@@ -1,74 +1,48 @@
-import { randomNumberInRange } from '../../index.js';
+import { brainGamesCore, randomNumberInRange } from '../index.js';
 
-export const rulesOfGame = 'What number is missing in the progression?';
+const rulesOfGame = 'What number is missing in the progression?';
 
-const removeCommas = (string) => {
-  let result = '';
-  for (let i = 0; i < string.length; i += 1) {
-    result += string[i].replace(',', ' ');
+const getArrayQuestionAnswer = () => {
+  const removeCommas = (string) => {
+    let result = '';
+    for (let i = 0; i < string.length; i += 1) {
+      result += string[i].replace(',', ' ');
+    }
+    return result;
+  };
+
+  const firstNumberRange = 1;
+  const lastNumberRange = 25;
+  let randomNumber = randomNumberInRange(firstNumberRange, lastNumberRange);
+
+  const firstNumberStepRange = 2;
+  const lastNumberStepRange = 5;
+  const randomStep = randomNumberInRange(firstNumberStepRange, lastNumberStepRange);
+
+  const firstNumberLengthRange = 5;
+  const lastNumberLengthRange = 10;
+  const lengthNumbers = randomNumberInRange(firstNumberLengthRange, lastNumberLengthRange);
+
+  const arrayNumbers = [];
+  for (let i = 0; i < lengthNumbers; i += 1) {
+    randomNumber += randomStep;
+    arrayNumbers[i] = randomNumber;
   }
-  return result;
+
+  const firstIndexRange = 0;
+  const lastIndexRange = lengthNumbers - 1;
+  const randomIndexColon = randomNumberInRange(firstIndexRange, lastIndexRange);
+
+  const correctNumber = arrayNumbers[randomIndexColon];
+  arrayNumbers[randomIndexColon] = '..';
+
+  const question = removeCommas(arrayNumbers.toString());
+  const answer = correctNumber.toString();
+
+  const arrayQuestionAnswer = [question, answer];
+  return arrayQuestionAnswer;
 };
 
-const firstNumberRange = 1;
-const lastNumberRange = 25;
-let randomNumber = randomNumberInRange(firstNumberRange, lastNumberRange);
+const runGameProgression = () => brainGamesCore(rulesOfGame, getArrayQuestionAnswer);
 
-const firstNumberStepRange = 2;
-const lastNumberStepRange = 5;
-let randomStep = randomNumberInRange(firstNumberStepRange, lastNumberStepRange);
-
-const firstNumberLengthRange = 5;
-const lastNumberLengthRange = 10;
-let lengthNumbers = randomNumberInRange(firstNumberLengthRange, lastNumberLengthRange);
-
-const firstArrayNumbers = [];
-for (let i = 0; i < lengthNumbers; i += 1) {
-  randomNumber += randomStep;
-  firstArrayNumbers[i] = randomNumber;
-}
-
-const firstIndexRange = 0;
-let lastIndexRange = lengthNumbers - 1;
-let randomIndexColon = randomNumberInRange(firstIndexRange, lastIndexRange);
-
-let correctNumber = firstArrayNumbers[randomIndexColon];
-firstArrayNumbers[randomIndexColon] = '..';
-
-const questionOne = removeCommas(firstArrayNumbers.toString());
-const answerOne = correctNumber.toString();
-
-const secondArrayNumbers = [];
-randomNumber = randomNumberInRange(firstNumberRange, lastNumberRange);
-randomStep = randomNumberInRange(firstNumberStepRange, lastNumberStepRange);
-lengthNumbers = randomNumberInRange(firstNumberLengthRange, lastNumberLengthRange);
-lastIndexRange = lengthNumbers - 1;
-for (let i = 0; i < lengthNumbers; i += 1) {
-  randomNumber += randomStep;
-  secondArrayNumbers[i] = randomNumber;
-}
-randomIndexColon = randomNumberInRange(firstIndexRange, lastIndexRange);
-correctNumber = secondArrayNumbers[randomIndexColon];
-secondArrayNumbers[randomIndexColon] = '..';
-
-const questionTwo = removeCommas(secondArrayNumbers.toString());
-const answerTwo = correctNumber.toString();
-
-const thirdArrayNumbers = [];
-randomNumber = randomNumberInRange(firstNumberRange, lastNumberRange);
-randomStep = randomNumberInRange(firstNumberStepRange, lastNumberStepRange);
-lengthNumbers = randomNumberInRange(firstNumberLengthRange, lastNumberLengthRange);
-lastIndexRange = lengthNumbers - 1;
-for (let i = 0; i < lengthNumbers; i += 1) {
-  randomNumber += randomStep;
-  thirdArrayNumbers[i] = randomNumber;
-}
-randomIndexColon = randomNumberInRange(firstIndexRange, lastIndexRange);
-correctNumber = thirdArrayNumbers[randomIndexColon];
-thirdArrayNumbers[randomIndexColon] = '..';
-
-const questionThree = removeCommas(thirdArrayNumbers.toString());
-const answerThree = correctNumber.toString();
-
-export const arrayQuestions = [questionOne, questionTwo, questionThree];
-export const arrayAnswers = [answerOne, answerTwo, answerThree];
+export default runGameProgression;
