0ae1426e3271cbc5369f471f0b07f7489e74065f
TIMUR <otto@MacBook-Air-Timur.local>
2022-08-30 17:07:10 +0300

    Fix src/games/game-prime.js

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -1,41 +1,32 @@
-import { brainGamesCore, randomNumberInRange } from '../index.js';
+import getRandomNumberInRange from '../helpers.js';
+import { runGameCore, lowerLimitInRange, upperLimitInRange } from '../index.js';
 
 const rulesOfGame = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-const getArrayQuestionAnswer = () => {
-  const getPrimeNumber = (number) => {
-    let result = 0;
+const getPrimeNumber = (number) => {
+  let divisionСounter = 0;
 
-    for (let i = number; i > 0; i -= 1) {
-      if (number % i === 0) {
-        result += 1;
-      }
+  for (let i = number; i > 0; i -= 1) {
+    if (number % i === 0) {
+      divisionСounter += 1;
     }
-
-    if (result === 2) {
-      return true;
-    }
-    return false;
-  };
-
-  const firstNumberRange = 1;
-  const lastNumberRange = 25;
-  const randomNumber = randomNumberInRange(firstNumberRange, lastNumberRange);
-  const question = randomNumber;
-
-  let result;
-  if (getPrimeNumber(randomNumber) === true) {
-    result = 'yes';
-  } else if (getPrimeNumber(randomNumber) === false) {
-    result = 'no';
   }
 
-  const answer = result;
-
-  const arrayQuestionAnswer = [question, answer];
-  return arrayQuestionAnswer;
+  if (divisionСounter === 2) {
+    return true;
+  }
+  return false;
 };
 
-const runGamePrime = () => brainGamesCore(rulesOfGame, getArrayQuestionAnswer);
+const getQuestionAnswerPair = () => {
+  const randomNumber = getRandomNumberInRange(lowerLimitInRange, upperLimitInRange);
+  const question = randomNumber;
+
+  const correctAnswer = (getPrimeNumber(randomNumber) === true) ? 'yes' : 'no';
+
+  return [question, correctAnswer];
+};
+
+const runGamePrime = () => runGameCore(rulesOfGame, getQuestionAnswerPair);
 
 export default runGamePrime;
