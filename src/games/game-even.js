4a1f4adf7f740edb2d1a5e2386af5f95ac307d6c
TIMUR <otto@MacBook-Air-Timur.local>
2022-08-30 17:06:04 +0300

    Fix src/games/game-even.js

--- src/games/game-even.js
+++ src/games/game-even.js
@@ -1,26 +1,27 @@
-import { brainGamesCore, randomNumberInRange } from '../index.js';
+import getRandomNumberInRange from '../helpers.js';
+import { runGameCore, lowerLimitInRange, upperLimitInRange } from '../index.js';
 
 const rulesOfGame = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const getArrayQuestionAnswer = () => {
-  const firstNumberRange = 1;
-  const lastNumberRange = 25;
-  const randomNumber = randomNumberInRange(firstNumberRange, lastNumberRange);
-  const question = randomNumber;
-
-  let result;
-  if (randomNumber % 2 === 0) {
-    result = 'yes';
-  } else if (randomNumber % 2 !== 0) {
-    result = 'no';
+const getCorrectAnswer = (number) => {
+  let correctAnswer;
+  if (number % 2 === 0) {
+    correctAnswer = 'yes';
+  } else if (number % 2 !== 0) {
+    correctAnswer = 'no';
   }
-
-  const answer = result;
-
-  const arrayQuestionAnswer = [question, answer];
-  return arrayQuestionAnswer;
+  return correctAnswer;
 };
 
-const runGameEven = () => brainGamesCore(rulesOfGame, getArrayQuestionAnswer);
+const getQuestionAnswerPair = () => {
+  const randomNumber = getRandomNumberInRange(lowerLimitInRange, upperLimitInRange);
+  const question = randomNumber;
+
+  const correctAnswer = getCorrectAnswer(randomNumber);
+
+  return [question, correctAnswer];
+};
+
+const runGameEven = () => runGameCore(rulesOfGame, getQuestionAnswerPair);
 
 export default runGameEven;
