f0bf0161197d390dcd6032d2c0601a526fe51418
TIMUR <otto@MacBook-Air-Timur.local>
2022-08-30 17:04:18 +0300

    Fix src/index.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,25 @@
 import readlineSync from 'readline-sync';
 
-export const brainGamesCore = (rules, arrayQuestionAnswer) => {
+export const lowerLimitInRange = 1;
+export const upperLimitInRange = 25;
+
+export const runGameCore = (rules, questionAnswerPair) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(rules);
 
   const amountRounds = 3;
-  const questionIndex = 0;
-  const answerIndex = 1;
-  let array;
-
-  let userAnswer;
   for (let i = 0; i < amountRounds; i += 1) {
-    array = arrayQuestionAnswer();
+    const [question, correctAnswer] = questionAnswerPair();
+    console.log(`Question: ${question}`);
 
-    console.log(`Question: ${array[questionIndex]}`);
-    userAnswer = readlineSync.question('Your answer: ');
-    if (userAnswer === array[answerIndex]) {
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer === correctAnswer) {
       console.log('Correct!');
     } else {
-      return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${array[answerIndex]}'.\nLet's try again, ${userName}!`);
+      return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
     }
   }
   return console.log(`Congratulations, ${userName}!`);
 };
-
-export const randomNumberInRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
