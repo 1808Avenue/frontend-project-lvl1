7dc3af6b4d6f263ee01cec1dbffd578e97cc0b47
TIMUR <otto@MacBook-Air-Timur.local>
2022-08-31 19:04:51 +0300

    Fix src/games/game-even.js

--- src/games/game-even.js
+++ src/games/game-even.js
@@ -4,13 +4,10 @@ import { runGameCore, lowerLimitInRange, upperLimitInRange } from '../index.js';
 const rulesOfGame = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const getCorrectAnswer = (number) => {
-  let correctAnswer;
   if (number % 2 === 0) {
-    correctAnswer = 'yes';
-  } else if (number % 2 !== 0) {
-    correctAnswer = 'no';
+    return 'yes';
   }
-  return correctAnswer;
+  return 'no';
 };
 
 const getQuestionAnswerPair = () => {
